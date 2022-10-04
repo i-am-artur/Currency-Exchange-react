2dd9f9a1ad4cf8b6c4098464ef780cf7a705111b
Artur <adrenalin247@gmail.com>
2022-10-04 20:41:58 +0300

    routing for portfolio

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio/currency-converter">
       <Provider store={store}>
         <App />
       </Provider>
